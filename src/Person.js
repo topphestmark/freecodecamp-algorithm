22e89d21f927aad1868b357c1639c9313a1cb064
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-03-18 21:04:10 +0100

    Person that not worked out

--- src/Person.js
+++ src/Person.js
@@ -1,38 +1,59 @@
 var Person = function (firstAndLast) {
-  // Only change code below this line
-  // Complete the method below and implement the others similarly
+  let firstName = firstAndLast.split(" ")[0];
+  let lastName = firstAndLast.split(" ")[1];
+
   this.getFullName = function () {
-    return "";
+    return `${firstName} ${lastName}`;
   };
-  return firstAndLast;
+
+  this.getFirstName = function () {
+    return firstName;
+  };
+  this.getLastName = function () {
+    return lastName;
+  };
+  this.getFullName = function () {
+    return firstAndLast;
+  };
+
+  this.setFirstName = function (first) {
+    firstName = first;
+  };
+  this.setLastName = function (last) {
+    lastName = last;
+  };
+  this.setFullName = function (firstAndLast) {
+    firstName = firstAndLast.split(" ")[0];
+    lastName = firstAndLast.split(" ")[1];
+  };
 };
 
 var bob = new Person("Bob Ross");
-bob.getFullName();
+console.log(bob.getLastName());
 
-Object.keys(bob).length; // 6.
+// Object.keys(bob).length; // 6.
 
-bob instanceof Person; // true.
+// bob instanceof Person; // true.
 
-bob.firstName; // undefined.
+// bob.firstName; // undefined.
 
-bob.lastName; // undefined.
+// bob.lastName; // undefined.
 
-bob.getFirstName(); //   "Bob".
+// bob.getFirstName(); //   "Bob".
 
-bob.getLastName(); //   "Ross".
+// bob.getLastName(); //   "Ross".
 
-bob.getFullName(); //   "Bob" Ross.
+// bob.getFullName(); //   "Bob" Ross.
 
-bob.getFullName(); //   "Haskell Ross" after bob.setFirstName("Haskell").
+// bob.getFullName(); //   "Haskell Ross" after bob.setFirstName("Haskell").
 
-bob.getFullName(); //   "Haskell Curry" after bob.setLastName("Curry").
+// bob.getFullName(); //   "Haskell Curry" after bob.setLastName("Curry").
 
-bob.getFullName(); //   "Haskell Curry" after bob.setFullName("Haskell Curry").
+// bob.getFullName(); //   "Haskell Curry" after bob.setFullName("Haskell Curry").
 
-bob.getFirstName(); //   "Haskell" after bob.setFullName("Haskell Curry").
+// bob.getFirstName(); //   "Haskell" after bob.setFullName("Haskell Curry").
 
-bob.getLastName(); //   "Curry" after bob.setFullName("Haskell Curry").
+// bob.getLastName(); //   "Curry" after bob.setFullName("Haskell Curry").
 
 // ### Make a Person ###
 // Fill in the object constructor with the following methods below:
